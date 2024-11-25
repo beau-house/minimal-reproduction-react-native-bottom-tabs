26f3a6a61dfd82ba0c586a20dc5bff36740fb11c
beau-house <beau.r.house@gmail.com>
2024-11-24 22:59:15 -0800

    Cleanup

--- App.tsx
+++ App.tsx
@@ -1,24 +1,12 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NavigationContainer, DarkTheme, DefaultTheme } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import NativeTabNavigator from './src/NativeTabNavigator'
 
 const RootStack = createNativeStackNavigator()
 
-const rootNavDefaultTheme = {
-  ...DefaultTheme,
-  colors: {
-    primary: 'hsl(0, 0%, 62.8%)',
-    background: 'hsl(0, 0%, 20%)',
-    card: 'hsl(0, 0%, 8.5%)',
-    text: 'hsl(0, 0%, 100%)',
-    border: 'magenta',
-    notification: 'magenta',
-  },
-}
-
 export default function App() {
   return (
-    <NavigationContainer theme={rootNavDefaultTheme}>
+    <NavigationContainer>
       <RootStack.Navigator>
         <RootStack.Screen
           name="Tab"

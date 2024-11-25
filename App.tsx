e6e7716ec004902581bb58d35206bf3b2ac48b20
beau-house <beau.r.house@gmail.com>
2024-11-24 23:08:12 -0800

    Simplify example

--- App.tsx
+++ App.tsx
@@ -1,19 +1,44 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createNativeBottomTabNavigator } from '@bottom-tabs/react-navigation'
 import { NavigationContainer } from '@react-navigation/native'
-import NativeTabNavigator from './src/NativeTabNavigator'
+import ScreenOne from './src/ScreenOne'
+import ScreenTwo from './src/ScreenTwo'
 
-const RootStack = createNativeStackNavigator()
+const Tab = createNativeBottomTabNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <RootStack.Navigator>
-        <RootStack.Screen
-          name="Tab"
-          component={NativeTabNavigator}
-          options={{ headerShown: false }}
+      <Tab.Navigator
+        barTintColor={'hsl(0, 0%, 6%)'}
+        hapticFeedbackEnabled
+        ignoresTopSafeArea
+        screenOptions={{ lazy: false }}
+        scrollEdgeAppearance="transparent"
+        tabBarActiveTintColor={'red'}
+        tabBarInactiveTintColor={'rgb(155,155,155)'}
+        tabLabelStyle={{
+          fontFamily: 'FKGrotesk-Medium',
+          fontSize: 11,
+        }}
+        translucent={false}
+      >
+        <Tab.Screen
+          name="Screen One"
+          component={ScreenOne}
+          options={{
+            lazy: false,
+            tabBarIcon: () => ({ sfSymbol: 'person.fill' }),
+          }}
         />
-      </RootStack.Navigator>
+        <Tab.Screen
+          name="Screen Two"
+          component={ScreenTwo}
+          options={{
+            lazy: false,
+            tabBarIcon: () => ({ sfSymbol: 'book.fill' }),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
